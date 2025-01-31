caaaa49520b78f890e3c49551bc7aa04912822a5
Michael Harris <46806666+MichaelHarrisCode@users.noreply.github.com>
2025-01-31 13:43:40 -0800

    Fixed JSON indentations =(

--- init.lua
+++ init.lua
@@ -77,20 +77,10 @@ vim.api.nvim_create_autocmd("Filetype", {
 
 vim.api.nvim_create_autocmd("Filetype", {
   group = filetype_rules,
-  pattern = "javascript",
+  pattern = { "javascript", "json" },
   callback = function()
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
-    vim.opt_local.expandtab = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("Filetype", {
-  group = filetype_rules,
-  pattern = "json",
-  callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
     vim.opt_local.expandtab = true
   end,
 })
